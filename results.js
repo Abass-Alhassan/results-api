ea4c10f3cb704cc4d8d6cfe16466002cb0b3fa9b
Abass-Alhassan <icewrldspacely@gmail.com>
2022-04-06 08:09:55 -0700

    first commit

---
+++ results.js
@@ -0,0 +1,16 @@
+const results=[
+    { name:"Wendy Dari", class:"Form One", marks:8},
+    { name:"Winter Dan", class:"Form One", marks:7},
+    { name:"Andy ari", class:"Form One", marks:6},
+    { name:"Hardy Dai", class:"Form One", marks:6},
+    { name:"Woody leri", class:"Form One", marks:8},
+    { name:"Wend eri", class:"Form One", marks:8},
+    { name:"endy ler", class:"Form One", marks:8},
+    { name:"Weny lri", class:"Form One", marks:8},
+    { name:"Widy lei", class:"Form One", marks:8},
+    { name:"Wey leri", class:"Form One", marks:8},
+    { name:"Werdy jeri", class:"Form One", marks:8},
+    { name:"andy peri", class:"Form One", marks:8}
+]
+
+module.exports=results
